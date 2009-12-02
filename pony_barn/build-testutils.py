7b469cf5bb2256b0290b0c8c4afbf12c0f7dac88
Eric Holscher <eric@ericholscher.com>
2009-12-02 17:40:18 -0600

    Fix the test-utils build.
    This is a good example of how to run things out of a checkout instead of installing it using setup.py. This works well for projects that have their own 'test project' set up, instead of using the default django tests.py stuff.

--- pony_barn/build-testutils.py
+++ pony_barn/build-testutils.py
@@ -1,25 +1,21 @@
 import sys
-from base_django import DjangoBuild
+from base import BaseBuild
 from pony_build import client as pony
 
-class TestUtilsBuild(DjangoBuild):
+class PonyBuild(BaseBuild):
 
     def __init__(self):
-        super(TestUtilsBuild, self).__init__()
+        super(PonyBuild, self).__init__()
         self.repo_url = "git://github.com/ericholscher/django-test-utils"
         self.name = "django-test-utils"
-        self.package_name = 'test_app'
-        self.required = ['django']
-        self.installed_apps = ['test_utils', 'test_project.polls', 'test_project.test_app']
+        self.required = ['django', 'twill']
 
     def define_commands(self):
 
         self.commands = [ pony.GitClone(self.repo_url),
-                     pony.BuildCommand([self.context.python, 'setup.py', 'install'], name='Install'),
-                     pony.BuildCommand([self.context.djangoadmin, 'syncdb', '--noinput', '--settings', 'django_pony_test_settings'], name='Install'),
-                     pony.TestCommand([self.context.djangoadmin, 'test', self.package_name, '--settings', 'django_pony_test_settings'], name='run tests')
+                        pony.TestCommand([self.context.python, 'test_project/runtests.py', '--settings', 'test_project.settings'], name='run tests')
                      ]
 
 if __name__ == '__main__':
-    build = TestUtilsBuild()
+    build = PonyBuild()
     sys.exit(build.execute(sys.argv))
