8798a525cbb0c38f1c31a86dff81c6b2bae1ac42
Eric Holscher <eric@ericholscher.com>
2009-12-02 18:01:06 -0600

    Add a build for django_compressor.

---
+++ pony_barn/build-compressor.py
@@ -0,0 +1,21 @@
+import sys
+from base import BaseBuild
+from pony_build import client as pony
+
+class PonyBuild(BaseBuild):
+
+    def __init__(self):
+        super(PonyBuild, self).__init__()
+        self.repo_url = "git://github.com/mintchaos/django_compressor.git"
+        self.name = "django_compressor"
+        self.required = ['django']
+
+    def define_commands(self):
+
+        self.commands = [ pony.GitClone(self.repo_url),
+                        pony.TestCommand([self.context.python, 'tests/manage.py', 'test', '--settings', 'tests.settings'], name='run tests')
+                     ]
+
+if __name__ == '__main__':
+    build = PonyBuild()
+    sys.exit(build.execute(sys.argv))
