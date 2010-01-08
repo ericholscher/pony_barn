0ed3d94ec855f5e40969aba9898de383443c133b
Eric Holscher <eric@ericholscher.com>
2010-01-07 23:37:23 -0600

    Add base Hg support with a sample nose build.

---
+++ pony_barn/base_hg.py
@@ -0,0 +1,13 @@
+import sys
+from pony_barn import client as pony
+from base import BaseBuild
+
+class HgBuild(BaseBuild):
+
+    def define_commands(self):
+
+        self.commands = [
+            pony.HgClone(self.repo_url, egg=self.package_name),
+            pony.BuildCommand([self.context.python, 'setup.py', 'install'], name='Install'),
+            pony.TestCommand([self.context.python, 'setup.py', 'test'], name='Run tests', run_cwd=None),
+            ]
---
+++ pony_barn/build-nose.py
@@ -0,0 +1,15 @@
+import sys
+from pony_build import client as pony
+from base_hg import HgBuild
+
+class PonyBuild(HgBuild):
+
+    def __init__(self):
+        super(PonyBuild, self).__init__()
+        self.name = "nose"
+        self.repo_url = 'http://bitbucket.org/jpellerin/nose/overview/'
+        self.package_name = "nose"
+
+if __name__ == '__main__':
+    build = PonyBuild()
+    sys.exit(build.execute(sys.argv))
