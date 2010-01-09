eb112ba5d10c53d9de471f78bed0be5678fb5282
Eric Holscher <eric@ericholscher.com>
2010-01-09 22:33:53 +0000

    Update test utils build.

--- pony_barn/build-testutils.py
+++ pony_barn/build-testutils.py
@@ -8,11 +8,12 @@ class PonyBuild(BaseBuild):
         super(PonyBuild, self).__init__()
         self.repo_url = "git://github.com/ericholscher/django-test-utils"
         self.name = "django-test-utils"
+        self.package_name = "test_utils"
         self.required = ['django', 'twill', 'BeautifulSoup']
 
     def define_commands(self):
 
-        self.commands = [ pony.GitClone(self.repo_url),
+        self.commands = [ pony.GitClone(self.repo_url, egg=self.package_name),
                         pony.TestCommand([self.context.python, 'test_project/runtests.py', '--settings', 'test_project.settings'], name='run tests')
                      ]
 
